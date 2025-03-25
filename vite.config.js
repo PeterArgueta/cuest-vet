1b9eefe90b97b3215954c61ddb9b7f8f099c3692
PeterArgueta <peterarguedo@gmail.com>
2025-03-24 23:35:14 -0600

    new file:   dist/assets/index-84cb2db3.css 	new file:   dist/assets/index-dc2d2789.js 	new file:   dist/index.html 	new file:   node_modules/.cache/gh-pages/https!github.com!PeterArgueta!cuest-vet.git 	modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/cuestionario-veterinaria/", 
+  base: "/cuest-vet/", 
   plugins: [react()],
   json: {
     namedExports: true,
